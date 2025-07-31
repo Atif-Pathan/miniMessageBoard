03cb513ec55f93ea93a56c9f33b186adb9f1e436
Atif Pathan <atifkhan.p1@gmail.com>
2025-07-31 00:50:45 -0600

    Remove ordering from getAllMessages query
    The getAllMessages function no longer orders messages by the 'added' column. This change may affect the order in which messages are returned from the database.

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const pool = require("./pool");
 
 async function getAllMessages() {
-  const { rows } = await pool.query("SELECT * FROM messages ORDER BY added ASC");
+  const { rows } = await pool.query("SELECT * FROM messages");
   return rows;
 }
 
